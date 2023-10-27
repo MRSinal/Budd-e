c862b460fc6a6c0f659f1b260f0b1df6df47a345
jouchuki <95777300+jouchuki@users.noreply.github.com>
2023-10-27 16:52:17 +0200

    Budd-e GPT and EEG integration

---
+++ record_eeg.py
@@ -0,0 +1,61 @@
+import sys
+import os
+import numpy as np
+import time
+
+sys.path.append('C:/Users/user1/Documents/gtec/Unicorn Suite/Hybrid Black/Unicorn Python/Lib')
+import UnicornPy
+
+def connect_to_device():
+    deviceList = UnicornPy.GetAvailableDevices(True)
+
+    if len(deviceList) <= 0 or deviceList is None:
+        raise Exception("No device available. Please pair with a Unicorn first.")
+
+    print("Available devices:")
+    for i, device in enumerate(deviceList):
+        print("#%i %s" % (i, device))
+
+    deviceID = 0
+    if deviceID < 0 or deviceID > len(deviceList):
+        raise IndexError('The selected device ID is not valid.')
+
+    print("Trying to connect to '%s'." % deviceList[deviceID])
+    device = UnicornPy.Unicorn(deviceList[deviceID])
+    print("Connected to '%s'." % deviceList[deviceID])
+
+    return device
+
+def record_data(device_set, duration=60, frame_length=1):
+    device = device_set
+    TestsignaleEnabled = False
+    all_data = bytearray()
+    numberOfAcquiredChannels = device.GetNumberOfAcquiredChannels()
+    receiveBufferBufferLength = frame_length * numberOfAcquiredChannels * 4
+
+    device.StartAcquisition(TestsignaleEnabled)
+    print("Data acquisition started.")
+
+    numberOfGetDataCalls = int(duration * UnicornPy.SamplingRate / frame_length)
+    consoleUpdateRate = int((UnicornPy.SamplingRate / frame_length) / 25.0)
+    if consoleUpdateRate == 0:
+        consoleUpdateRate = 1
+
+    for i in range(0, numberOfGetDataCalls):
+        receiveBuffer = bytearray(receiveBufferBufferLength)
+        device.GetData(frame_length, receiveBuffer, receiveBufferBufferLength)
+        all_data.extend(receiveBuffer)  # Append data to the bytearray
+
+        if i % consoleUpdateRate == 0:
+            print('.', end='', flush=True)
+
+    device.StopAcquisition()
+    print("\nData acquisition stopped.")
+
+    # Convert bytearray to a flat numpy array of float32 values
+    flat_data = np.frombuffer(all_data, dtype=np.float32)
+    
+    # Reshape the flat array into a 2D array
+    reshaped_data = flat_data.reshape(-1, numberOfAcquiredChannels)
+    
+    return reshaped_data
---
+++ rf_ppl.py
@@ -0,0 +1,69 @@
+import numpy as np
+from sklearn.svm import SVC
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import pandas as pd
+import record_eeg
+import prepro
+
+import os 
+BASE_PATH = "C:/Users/user1/Documents/Downloads/HackathonGOOOD/Hackathon"
+folders = ["positive_processed", "negative_processed", "neutral_processed"]
+labels_map = {"positive_processed": 0, "negative_processed": 1, "neutral_processed": 2}
+    
+
+# Extract features from EEG data (for demonstration, let's compute mean and variance)
+def extract_features(data):
+    # Mean and variance as features (you can expand this to include more sophisticated features)
+    mean = np.mean(data)
+    var = np.var(data)
+    return np.hstack((mean, var))
+
+
+def load_and_process_data(BASE_PATH, folders):
+    """Loads the EEG data, processes it, and extracts features."""
+    X = []  # Features
+    y = []  # Labels
+
+    for folder in folders:
+        folder_path = os.path.join(BASE_PATH, folder)
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".npy"):
+                file_path = os.path.join(folder_path, filename)
+                data = np.load(file_path)
+
+                # Extract features
+                features = extract_features(data)
+
+                X.append(features)
+                y.append(labels_map[folder])
+
+    return np.array(X), np.array(y)
+
+def rf_training(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Initialize the Random Forest classifier
+    rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+
+    # Fit the model to the training data
+    rf_classifier.fit(X_train, y_train)
+
+    # Make predictions on the testing data
+    y_pred = rf_classifier.predict(X_test)
+
+    return rf_classifier
+
+def make_predictions(clf, z):
+    predicted_label = clf.predict(z.reshape(1, -1))
+    return predicted_label
+
+def rec_n_pred(device_set):
+    X, y = load_and_process_data(BASE_PATH, folders)
+    z = extract_features(prepro.preprocess_data(record_eeg.record_data(device_set)))
+    return make_predictions(rf_training(X, y), z)
